fef46fa1501b0b2ebbfaf859d4c9db665d71170d
girawhale <girawhale@naver.com>
2021-04-17 22:45:16 +0900

    15. Bad Request Error body 추가

--- src/main/java/com/restapi/demoinfleanrestapi/events/EventValidator.java
+++ src/main/java/com/restapi/demoinfleanrestapi/events/EventValidator.java
@@ -10,8 +10,9 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
-            errors.rejectValue("basePrice", "wrongValue", "Base price is wrong.");
-            errors.rejectValue("maxPrice", "wrongValue", "Max price is wrong.");
+//            errors.rejectValue("basePrice", "wrongValue", "Base price is wrong.");
+//            errors.rejectValue("maxPrice", "wrongValue", "Max price is wrong.");
+            errors.reject("wrongPrices", "Prices is wrong");
 
         }
 
@@ -25,10 +26,10 @@ public class EventValidator {
             errors.rejectValue("endEventDateTime", "wrongValue", "End Event DateTime is Wrong");
         }
 
-        if (beginEventDateTime.isAfter(endEventDateTime)
-                || beginEventDateTime.isAfter(closeEnrollmentDateTime)) {
-            errors.rejectValue("BeginEventDateTime", "wrongValue", "Begin Event DateTime is Wrong");
-        }
+//        if (beginEventDateTime.isAfter(endEventDateTime)
+//                || beginEventDateTime.isAfter(closeEnrollmentDateTime)) {
+//            errors.rejectValue("BeginEventDateTime", "wrongValue", "Begin Event DateTime is Wrong");
+//        }
 
     }
 }
