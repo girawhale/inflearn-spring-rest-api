6bde45e053e46a983e1c7b08a2768f8df5f86dcc
girawhale <girawhale@naver.com>
2021-04-19 21:05:11 +0900

    35. oauth2 인증 서버 설정

---
+++ src/test/java/com/restapi/demoinfleanrestapi/configs/AuthServerConfigTest.java
@@ -0,0 +1,49 @@
+package com.restapi.demoinfleanrestapi.configs;
+
+
+import com.restapi.demoinfleanrestapi.accounts.AccountService;
+import com.restapi.demoinfleanrestapi.common.BaseControllerTest;
+import com.restapi.demoinfleanrestapi.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        // Given
+        String username = "girawhale@naver.com";
+        String password = "password";
+//        Account account = Account.builder()
+//                .email(username)
+//                .password(password)
+//                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+//                .build();
+//        accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                .with(httpBasic(clientId, clientSecret))
+                .param("username", username)
+                .param( "password", password)
+                .param("grant_type", "password")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists())
+        ;
+    }
+
+}
